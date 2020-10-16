602209cee7d4f52e4a0c64fae4a8360f976c2718
nguyentrungnghia2000 <58599798+nguyentrungnghia2000@users.noreply.github.com>
2020-10-16 14:16:36 +0700

    add file upload

---
+++ BlasterMaster/PlayScence.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Game.h"
+#include "Textures.h"
+#include "Scence.h"
+#include "GameObject.h"
+#include "Brick.h"
+#include "Mario.h"
+#include "Goomba.h"
+#include "Koopas.h"
+#include "Car.h"
+
+
+class CPlayScene : public CScene
+{
+protected:
+	//CMario* player;					// A play scene has to have player, right? 
+	CCar* player;
+
+	vector<LPGAMEOBJECT> objects;
+
+	void _ParseSection_TEXTURES(string line);
+	void _ParseSection_SPRITES(string line);
+	void _ParseSection_ANIMATIONS(string line);
+	void _ParseSection_ANIMATION_SETS(string line);
+	void _ParseSection_OBJECTS(string line);
+
+
+public:
+	CPlayScene(int id, LPCWSTR filePath);
+
+	virtual void Load();
+	virtual void Update(DWORD dt);
+	virtual void Render();
+	virtual void Unload();
+
+	//CMario* GetPlayer() { return player; }
+	CCar* GetPlayer() { return player; }
+
+	//friend class CPlayScenceKeyHandler;
+};
+
+class CPlayScenceKeyHandler : public CScenceKeyHandler
+{
+	bool up = true;
+public:
+	virtual void KeyState(BYTE* states);
+	virtual void OnKeyDown(int KeyCode);
+	virtual void OnKeyUp(int KeyCode);
+	CPlayScenceKeyHandler(CScene* s) :CScenceKeyHandler(s) {};
+};
+
